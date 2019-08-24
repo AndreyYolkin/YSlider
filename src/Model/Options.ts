db6a887f8171c794d3162c7b97ae54484290e773
Andrey Yolkin <andreyyolkin@gmail.com>
2019-08-24 17:54:18 +0700

    Added options for model of slider

---
+++ src/Model/Options.ts
@@ -0,0 +1,13 @@
+export default interface Options {
+  values: number | Array<number> | string;
+  range: Array<number> | Array<string>;
+  step?: number;
+  orientation?: orientation;
+  displaySteps?: boolean;
+  displayBubbles?: boolean;
+}
+
+export const enum orientation {
+  vertical,
+  horizontal
+}
