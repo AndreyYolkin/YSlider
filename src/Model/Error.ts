38440d01f9bd6a676ef2bbbf3cfbd9cac507eef3
Andrey Yolkin <andreyyolkin@gmail.com>
2019-09-26 11:06:25 +0300

    Added toString nethon, new error types, bindings

--- src/Model/Error.ts
+++ src/Model/Error.ts
@@ -5,28 +5,44 @@ export default class ErrorBuilder extends Error {
     const lasterror = ErrorBuilder.getError("" + types.pop());
     const errors = types.map(errtype => ErrorBuilder.getError(errtype));
     let msg;
-    msg = `Error in key '${key}': value must be ${
-      (types.length
-        ? errors.join(", ") + " or "
-        : ""
-      ) 
-      + lasterror}`;
+    msg = `Error in key '${key}': value must be ${(types.length
+      ? errors.join(", ") + " or "
+      : "") + lasterror}`;
     super(msg);
     this._error = msg;
     this._types = types;
+    this.getError = this.getError.bind(this);
+    this.getTypes = this.getTypes.bind(this);
+
+    Object.setPrototypeOf(this, ErrorBuilder.prototype);
   }
   private static getError(type: string) {
     const ErrorTypes: Record<string, string> = {
+      /*Common errors*/
+      common: "a valid type",
+      nonConfig: "any possible type, but key isn't found in config or invalid",
+
+      /*Types*/
       number: "a number",
       string: "a string",
       boolean: "a boolean",
       arrayOfNumbers: "an array of numbers",
-      arrayOfStrings: "an array of strings"
+      arrayOfStrings: "an array of strings",
+      arrayOfBooleans: "an array of booleans",
+      orientation: "\"horizontal\" or \"vertical\"",
+      
+      /*Validation Errors*/
+      range: "in range",
+      positive: "positive",
     };
     if (ErrorTypes[type]) {
       return ErrorTypes[type];
     }
     return;
+  }
+
+  toString() {
+    return this.getError();
   }
 
   getError() {
