c6779824d5a22f5ebe6fc989331d525f2e066602
Andrey Yolkin <andreyyolkin@gmail.com>
2019-10-19 13:22:02 +0300

    Update build dependencies, added stylelint

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,83 @@
 const webpack = require('webpack')
 const path = require('path')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const babelLoader = {
+  loader: 'babel-loader',
+  options: {
+    cacheDirectory: true,
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          targets: {
+            node: 'current',
+          },
+        },
+      ],
+      '@babel/preset-typescript',
+    ],
+  }
+};
 
 module.exports = [{
   name: 'client',
   mode: 'production',
   context: path.resolve(__dirname),
-  entry: {
-    javascript: ['babel-polyfill', './src/index.js'],
-    typescript: './src/index.ts'
-  },
+  entry: './src/entry/plugin.ts',
   module: {
     rules: [
       {
+        test: /\.pug$/,
+        loader: "pug-loader",
+        options: {
+          pretty: true
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: { sourceMap: true }
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      },
+      {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: babelLoader,
         exclude: /node_modules/
       }
     ]
   },
   resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ]
+    extensions: ['.tsx', '.ts', '.js']
   },
   output: {
-    filename: 'bundle.js',
+    filename: 'build.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  externals: {
+    jquery: 'jQuery'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: false,
+      template: "./src/entry/index.pug",
+      filename: "index.html",
+      inject: "head"
+    }),
+    /*new webpack.ProvidePlugin({
+      $: "jquery",
+      "window.jQuery": "jquery",
+      jQuery: "jquery"
+    }),*/
+  ]
 }]
