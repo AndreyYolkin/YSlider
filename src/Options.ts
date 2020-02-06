f40c7236eef2e8885c414348dbb55bdf5a920eec
Andrey Yolkin <andreyyolkin@gmail.com>
2020-02-06 13:11:32 +0300

    clean Options

--- src/Options.ts
+++ src/Options.ts
@@ -1,16 +1,16 @@
 export interface Options {
-  values: number | Array<number> //| string;
-  range: Array<number> //| Array<string>;
-  connects?: boolean | Array<boolean>;
-  step?: number;
-  orientation?: "horizontal" | "vertical";
-  displaySteps?: boolean;
-  displayBubbles?: boolean;
+  values: number | Array<number>;
+  range: Array<number>;
+  connects: boolean | Array<boolean>;
+  step: number;
+  orientation: "horizontal" | "vertical";
+  displaySteps: boolean;
+  displayBubbles: boolean;
   [key: string]: any;
 }
 
 export const defaults: Options = {
-  values: [10, 30, 80],
+  values: 0,
   range: [0, 100],
   connects: true,
   step: 1,
