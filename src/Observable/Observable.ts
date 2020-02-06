2027c771727d4c48e155522d8abce2feb3a12ca9
Andrey Yolkin <andreyyolkin@gmail.com>
2020-02-06 14:00:55 +0300

    Observers can now remove all subscriptions of selected type

--- src/Observable/Observable.ts
+++ src/Observable/Observable.ts
@@ -18,8 +18,13 @@ class Observable {
     if (!this._observers[type]) this._observers[type] = [];
     this._observers[type].push(callback);
   }
-  unsubscribe(type: string, callback: Function) {
-    this._observers[type] = this._observers[type].filter(el => el !== callback);
+  unsubscribe(type: string, callback?: Function) {
+    if (typeof callback !== "undefined"){
+      this._observers[type] = this._observers[type].filter(el => el !== callback);
+    }
+    else {
+      this._observers[type] = [];
+    }
   }
   emit(type: string, data?: any) {
     if (this._observers.hasOwnProperty(type)) {
