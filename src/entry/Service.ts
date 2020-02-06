ea9e47889d3598b4c56a4d07a7357a3b75259cb6
Andrey Yolkin <andreyyolkin@gmail.com>
2020-02-06 14:01:53 +0300

    Implemented Plugin methods

--- src/entry/Service.ts
+++ src/entry/Service.ts
@@ -1,15 +1,38 @@
 import Model from "../Model/Model";
-import Slider from "../View/Slider/Slider";
+import { Slider as View } from "../View/Slider/Slider";
 import { Presenter } from "../Presenter/Presenter";
 import Options from "../Options";
 
-export class YSlider {
-    model: Model;
-    view: Slider;
-    presenter: Presenter;
-    constructor(options: Options, root: JQuery<HTMLElement>) {
-        this.model = new Model(options);
-        this.view = new Slider(root);
-        this.presenter = new Presenter(this.model, this.view);
+export class _YSlider {
+  model: Model;
+  presenter: Presenter;
+  constructor(options: Options, root?: HTMLElement) {
+    this.model = new Model(options);
+    this.presenter = new Presenter(this.model);
+    if (typeof root !== "undefined") {
+      this.presenter.addView(root, options);
     }
+    return this;
+  }
+  addView(root: HTMLElement, options: Options) {
+    this.presenter.addView(root, options);
+  }
+  removeView(root: HTMLElement) {
+    this.presenter.removeView(root)
+  }
+  draw() {
+    this.presenter.updateView()
+  }
+  get() {
+      return this.presenter.get()
+  }
+  set(options: Partial<Options>) {
+      this.presenter.set(options);
+  }
+  on(event: string, callback: Function) {
+      this.presenter.subscribe(event, callback);
+  }
+  off(event: string, callback?: Function){
+    this.presenter.unsubscribe(event, callback)
+  }
 }
--- src/entry/plugin.ts
+++ src/entry/plugin.ts
@@ -1,23 +1,87 @@
 import $ from "jquery";
 import { Options, defaults } from "../Options";
-import { YSlider } from "./Service";
+import { _YSlider } from "./Service";
 
-// Define the plugin function on the jQuery extension point.
-// Note: Function and global default options must be combined as long as the options are mandatory.
-$.fn.YSlider = Object.assign<YSliderFunction, YSliderGlobalOptions>(
-  function(this: JQuery, options: Options): JQuery {
-    // Merge the global options with the options given as argument.
-    options = $.extend({}, $.fn.YSlider.options, options);
+declare global {
+  interface YSlider {
+    (options: Options): JQuery;
+  }
 
-    // Do what the plugin should do. Here we create an instance of the separate service which is then used when the
-    // user clicks the element that the plugin is attached to. It produces a greeting message and appends it to the output.
-    let yslider = new YSlider(options, this);
+  interface JQuery {
+    YSlider: YSlider;
+    yslider: YSlider;
+  }
 
-    return this;
-  },
-  {
-    options: {
-      ...defaults
+  interface SupportedFunctions {
+    get: Function;
+    set: Function;
+    draw: Function;
+    on: Function;
+    off: Function;
+    addView: Function;
+    removeView: Function;
+    [key: string]: any;
+  }
+}
+
+$.fn.yslider = $.fn.YSlider = function(
+  this: JQuery,
+  options: Partial<Options> | string,
+  ...content: any
+) {
+  let yslider: _YSlider;
+  if (!!this.data("yslider")) {
+    yslider = this.data("yslider");
+  }
+  const functions: SupportedFunctions = {
+    get: () => {
+      return yslider.get();
+    },
+    set: (options: Partial<Options>) => {
+      yslider.set(options);
+      return this;
+    },
+    draw: () => {
+      yslider.draw();
+      return this;
+    },
+    on: (event: string, action: Function) => {
+      yslider.on(event, action);
+      return this;
+    },
+    off: (event: string, action?: Function) => {
+      yslider.off(event, action);
+      return this;
+    },
+    addView: (
+      target: HTMLElement | JQuery<HTMLElement>,
+      options: Partial<Options>
+    ) => {
+      $(target).each((_, a) => {
+        yslider.addView(a, { ...defaults, ...options });
+        $(a).data("yslider", yslider);
+      });
+    },
+    removeView: (target: HTMLElement | JQuery<HTMLElement>) => {
+      $(target).each((_, a) => yslider.removeView(a));
+    }
+  };
+  if (!!this.data("yslider")) {
+    if (typeof options === "undefined") {
+      return functions;
+    }
+    if (typeof options === "string") {
+      if (functions.hasOwnProperty(options)) {
+        return functions[options].apply(this.data("yslider"), content);
+      }
     }
   }
-);
+  if (typeof options === "object" && options !== null) {
+    options = { ...defaults, ...options };
+    let values: Options = $.extend({}, defaults, options);
+    yslider = new _YSlider(values);
+    this.data("yslider", yslider);
+    this.map(el => yslider.addView(this[el], values));
+    return functions;
+  }
+};
