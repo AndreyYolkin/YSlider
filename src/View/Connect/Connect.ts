a5bd21d5c97b84d3996dcede08f5810fe86493e0
Andrey Yolkin <andreyyolkin@gmail.com>
2020-02-24 00:56:29 +0300

    Add Event emitter for click

--- src/View/Connect/Connect.ts
+++ src/View/Connect/Connect.ts
@@ -2,18 +2,52 @@ import { _createElement } from "../../tools";
 import Observable from "../../Observable/Observable";
 import "./Connect.scss";
 
+interface ClickArea {
+  none: boolean;
+  top: boolean;
+  left: boolean;
+}
+
 export class Connect extends Observable {
   readonly connect: HTMLElement;
   visible: boolean = true;
   flexGrow!: number;
+  clickArea: ClickArea;
 
   constructor() {
     super();
     this.draw = this.draw.bind(this);
+    this.onClicked = this.onClicked.bind(this);
+    this.clickArea = {
+      none: true,
+      left: false,
+      top: false
+    };
     this.connect = _createElement("div", {
       class:
         "y-slider__connect" + (this.visible ? " y-slider__connect-visible" : "")
     });
+    this.connect.addEventListener("click", this.onClicked);
+  }
+
+  onClicked(event: MouseEvent) {
+    let clickArea: ClickArea;
+    if (event.target !== null) {
+      this.clickArea = {
+        none: false,
+        //@ts-ignore
+        left: event.offsetX < event.target.offsetWidth / 2,
+        //@ts-ignore
+        top: event.offsetY < event.target.offsetHeight / 2
+      };
+    } else {
+      this.clickArea = {
+        none: true,
+        left: false,
+        top: false
+      };
+    }
+    this.emit("connectClicked")
   }
 
   static getArrayOfVisibiliteies(
